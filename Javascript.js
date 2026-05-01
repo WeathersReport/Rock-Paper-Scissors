7e789dd82a800c38e27e0e43f941760ad7b667f1
WeathersReport <188347858+WeathersReport@users.noreply.github.com>
2026-05-01 14:49:19 +0000

    Add function to play one round

--- Javascript.js
+++ Javascript.js
@@ -3,11 +3,11 @@ function getComputerChoice()
 let num = Math.floor(Math.random() * 3);
 let choice;
 switch(num){
-case 0: choice = "Rock"
+case 0: choice = "rock"
 break;
-case 1: choice = "Paper"
+case 1: choice = "paper"
 break;
-case 2: choice = "Scissors"
+case 2: choice = "scissors"
 break;
 }
 return choice;
@@ -21,12 +21,52 @@ return humanChoice;
 
 let humanScore = 0;
 let computerScore = 0;
-console.log(getComputerChoice());
-console.log(getHumanChoice());
 
 function playRound(humanChoice,computerChoice)
 {
-    
+    humanChoice.toLowerCase();
+    if (humanChoice == "rock" && computerChoice == "rock")
+    {
+         console.log("It's a tie!");
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper")
+    {
+        console.log("You lose! Paper beats rock!");
+        computerScore++;
+    }
+      else if (humanChoice == "rock" && computerChoice == "scissors")
+    {
+        console.log("You win! Rock beats Scissors!");
+        humanScore++;
+    }
+      else if  (humanChoice == "paper" && computerChoice == "paper")
+    {
+        console.log("It's a tie!");
+    }
+      else if  (humanChoice == "paper" && computerChoice == "rock")
+    {
+        console.log("You win! Paper beats rock!");
+        humanScore++;
+    }
+      else if  (humanChoice == "paper" && computerChoice == "scissors")
+    {
+        console.log("You lose! Scissors beat Paper!");
+        computerScore++;
+    }
+      else if  (humanChoice == "scissors" && computerChoice == "paper")
+    {
+        console.log("You win! Scissors beat Paper!");
+        humanScore++;
+    }
+      else if  (humanChoice == "scissors" && computerChoice == "scissors")
+    {
+        console.log("It's a tie!");
+    }
+      else if  (humanChoice == "scissors" && computerChoice == "rock")
+    {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
 }
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
