735b44ea7405f664e28c4e73251802e249311ec5
WeathersReport <188347858+WeathersReport@users.noreply.github.com>
2026-05-01 14:21:12 +0000

    Add switch case to choose between 3 choices Changed variable names

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,16 @@
 function getComputerChoice(){
-let choice = Math.floor(Math.random() * 3);
+let num = Math.floor(Math.random() * 3);
+let choice;
+switch(num){
+case 0: choice = "Rock"
+break;
+case 1: choice = "Paper"
+break;
+case 2: choice = "Scissors"
+break;
+default:
+console.log("Wrong input!")
+}
+console.log(num);
+console.log(choice);
 }
